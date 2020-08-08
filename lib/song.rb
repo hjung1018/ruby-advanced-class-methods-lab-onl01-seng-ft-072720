1432864aeec1e0c910b2087ce8caccbf0642707b
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-08 08:45:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,36 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{ |song| song.name == name }
+  end
+  
+  def self.find_or_create_by_name(name)
+    if @@all.find { |song| song.name == false }
+      song = self.new
+      song
+    else
+      @all.find{ |song| song.name == name }
+  end
+end
 
 end
